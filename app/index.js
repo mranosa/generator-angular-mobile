24c81480555281ccba41e3c9e9afc71a56a0e163
Mark Kenneth Ranosa <mark.ranosa@gmail.com>
2014-10-14 11:21:08 +0800

    Added ng-component

--- app/index.js
+++ app/index.js
@@ -1,15 +1,26 @@
+// https://github.com/DaftMonk/generator-angular-fullstack/blob/master/util.js
 'use strict';
+
 var util = require('util');
 var path = require('path');
 var yeoman = require('yeoman-generator');
 var yosay = require('yosay');
+var genUtils = require('../util.js');
 
 var AngularMobileGenerator = yeoman.generators.Base.extend({
-  initializing: function () {
+  initializing: function() {
+    this.argument('name', { type: String, required: false });
+    this.appname = this.name || path.basename(process.cwd());
+    this.appname = this._.camelize(this._.slugify(this._.humanize(this.appname)));
+
+    this.scriptAppName = this.appname + genUtils.appName(this);
+    this.appPath = this.env.options.appPath;
     this.pkg = require('../package.json');
+
+    this.filters = {};
   },
 
-  prompting: function () {
+  prompting: function() {
     var done = this.async();
 
     // Have Yeoman greet the user.
@@ -18,35 +29,82 @@ var AngularMobileGenerator = yeoman.generators.Base.extend({
     ));
 
     var prompts = [{
-      type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
-      default: true
+      type: "list",
+      name: "router",
+      default: 1,
+      message: "What Angular router would you like to use?",
+      choices: ["ngRoute", "uiRouter"],
+      filter: function(val) {
+        return val.toLowerCase();
+      }
     }];
 
-    this.prompt(prompts, function (props) {
-      this.someOption = props.someOption;
+    this.prompt(prompts, function(answers) {
+      this.filters[answers.router] = true;
 
       done();
     }.bind(this));
   },
 
-  writing: {
-    app: function () {
-      this.dest.mkdir('app');
-      this.dest.mkdir('app/templates');
+  saveSettings: function() {
+    if (this.skipConfig) return;
+    this.config.set('insertRoutes', true);
+    this.config.set('registerRoutesFile', 'server/routes.js');
+    this.config.set('routesNeedle', '// Insert routes below');
 
-      this.src.copy('_package.json', 'package.json');
-      this.src.copy('_bower.json', 'bower.json');
-    },
+    this.config.set('routesBase', '/api/');
+    this.config.set('pluralizeRoutes', true);
 
-    projectfiles: function () {
-      this.src.copy('editorconfig', '.editorconfig');
-      this.src.copy('jshintrc', '.jshintrc');
-    }
+    this.config.set('insertSockets', true);
+    this.config.set('registerSocketsFile', 'server/config/socketio.js');
+    this.config.set('socketsNeedle', '// Insert sockets below');
+
+    this.config.set('filters', this.filters);
+    this.config.forceSave();
   },
 
-  end: function () {
+  compose: function() {
+    var appPath = 'www/app/';
+    var filters = [];
+
+    if (this.filters.ngroute) filters.push('ngroute');
+    if (this.filters.uirouter) filters.push('uirouter');
+
+    this.composeWith('ng-component', {
+      options: {
+        'routeDirectory': appPath,
+        'directiveDirectory': appPath,
+        'filterDirectory': appPath,
+        'serviceDirectory': appPath,
+        'filters': filters,
+        'basePath': 'www'
+      }
+    }, {
+      local: require.resolve('generator-ng-component/app/index.js')
+    });
+  },
+
+  ngModules: function() {
+    this.filters = this._.defaults(this.config.get('filters'));
+
+    var angModules = [
+      "'ngCookies'",
+      "'ngResource'",
+      "'ngSanitize'"
+    ];
+
+    if (this.filters.ngroute) angModules.push("'ngRoute'");
+    if (this.filters.uirouter) angModules.push("'ui.router'");
+
+    this.angularModules = "\n  " + angModules.join(",\n  ") + "\n";
+  },
+
+  generate: function() {
+    this.sourceRoot(path.join(__dirname, './templates'));
+    genUtils.processDirectory(this, '.', '.');
+  },
+
+  end: function() {
     this.installDependencies();
   }
 });
